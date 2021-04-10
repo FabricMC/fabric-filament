99aecd1aff1ff03129f6e7e2c3b807df9f835142
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-04-10 23:59:36 +0300

    Add javadoc lint task (#2)
    * Add javadoc lint task
    
    * Add tests for JavadocLintTask
    
    * Add test for multiple incorrect mapping files
    
    * Clean up JavadocLintTest
    
    * Update Enigma
    
    * Make the javadoc lint task incremental and throw it on a work queue
    
    * Bump version to 0.2.0

--- src/main/java/net/fabricmc/filament/FilamentGradlePlugin.java
+++ src/main/java/net/fabricmc/filament/FilamentGradlePlugin.java
@@ -4,10 +4,12 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 import net.fabricmc.filament.task.GeneratePackageInfoMappingsTask;
+import net.fabricmc.filament.task.JavadocLintTask;
 
 public final class FilamentGradlePlugin implements Plugin<Project> {
 	@Override
 	public void apply(Project project) {
 		project.getTasks().register("generatePackageInfoMappings", GeneratePackageInfoMappingsTask.class);
+		project.getTasks().register("javadocLint", JavadocLintTask.class);
 	}
 }
