dda453ff425c1fb1ad89e07867bc025f81955fed
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-04-10 19:57:53 +0300

    Clean up JavadocLintTest

--- src/test/java/net/fabricmc/filament/test/JavadocLintTest.java
+++ src/test/java/net/fabricmc/filament/test/JavadocLintTest.java
@@ -8,15 +8,19 @@ import org.gradle.testkit.runner.TaskOutcome;
 import org.junit.jupiter.api.Test;
 
 class JavadocLintTest extends ProjectTest {
+	private BuildResult runGradleBuild(boolean shouldSucceed) {
+		GradleRunner runner = GradleRunner.create()
+				.withPluginClasspath()
+				.withProjectDir(projectDirectory)
+				.withArguments("javadocLint");
+
+		return shouldSucceed ? runner.build() : runner.buildAndFail();
+	}
+
 	@Test
 	public void paramInMethod() {
 		setupProject("javadocLint", "mappings/ParamInMethod.mapping");
-
-		BuildResult result = GradleRunner.create()
-				.withPluginClasspath()
-				.withProjectDir(projectDirectory)
-				.withArguments("javadocLint")
-				.buildAndFail();
+		BuildResult result = runGradleBuild(false);
 
 		assertThat(result.task(":javadocLint").getOutcome()).isEqualTo(TaskOutcome.FAILED);
 		assertThat(result.getOutput()).contains("method javadoc contains parameter docs");
@@ -25,12 +29,7 @@ class JavadocLintTest extends ProjectTest {
 	@Test
 	public void periodInParam() {
 		setupProject("javadocLint", "mappings/ParamPeriod.mapping");
-
-		BuildResult result = GradleRunner.create()
-				.withPluginClasspath()
-				.withProjectDir(projectDirectory)
-				.withArguments("javadocLint")
-				.buildAndFail();
+		BuildResult result = runGradleBuild(false);
 
 		assertThat(result.task(":javadocLint").getOutcome()).isEqualTo(TaskOutcome.FAILED);
 		assertThat(result.getOutput()).contains("parameter javadoc ends with '.'");
@@ -39,12 +38,7 @@ class JavadocLintTest extends ProjectTest {
 	@Test
 	public void uppercaseParam() {
 		setupProject("javadocLint", "mappings/UppercaseParam.mapping");
-
-		BuildResult result = GradleRunner.create()
-				.withPluginClasspath()
-				.withProjectDir(projectDirectory)
-				.withArguments("javadocLint")
-				.buildAndFail();
+		BuildResult result = runGradleBuild(false);
 
 		assertThat(result.task(":javadocLint").getOutcome()).isEqualTo(TaskOutcome.FAILED);
 		assertThat(result.getOutput()).contains("parameter javadoc starts with uppercase word 'The'");
@@ -58,12 +52,7 @@ class JavadocLintTest extends ProjectTest {
 				"mappings/ParamPeriod.mapping",
 				"mappings/UppercaseParam.mapping"
 		);
-
-		BuildResult result = GradleRunner.create()
-				.withPluginClasspath()
-				.withProjectDir(projectDirectory)
-				.withArguments("javadocLint")
-				.buildAndFail();
+		BuildResult result = runGradleBuild(false);
 
 		assertThat(result.task(":javadocLint").getOutcome()).isEqualTo(TaskOutcome.FAILED);
 		assertThat(result.getOutput()).contains("Found 3 javadoc format errors");
@@ -72,12 +61,7 @@ class JavadocLintTest extends ProjectTest {
 	@Test
 	public void successful() {
 		setupProject("javadocLint", "mappings/Successful.mapping");
-
-		BuildResult result = GradleRunner.create()
-				.withPluginClasspath()
-				.withProjectDir(projectDirectory)
-				.withArguments("javadocLint")
-				.build();
+		BuildResult result = runGradleBuild(true);
 
 		assertThat(result.task(":javadocLint").getOutcome()).isEqualTo(TaskOutcome.SUCCESS);
 	}
