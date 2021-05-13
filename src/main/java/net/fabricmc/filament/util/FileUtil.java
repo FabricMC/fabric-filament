2f570369bfe3337f3682baa216ae5e64f7c42066
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-05-13 13:33:40 +0300

    Revert to FileWriter for writing unpick definitions

--- src/main/java/net/fabricmc/filament/util/FileUtil.java
+++ src/main/java/net/fabricmc/filament/util/FileUtil.java
@@ -1,7 +1,9 @@
 package net.fabricmc.filament.util;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public final class FileUtil {
@@ -29,6 +31,8 @@ public final class FileUtil {
 	}
 
 	public static void write(File file, String content) throws IOException {
-		Files.writeString(file.toPath(), content);
+		try (FileWriter writer = new FileWriter(file, StandardCharsets.UTF_8)) {
+			writer.write(content);
+		}
 	}
 }
